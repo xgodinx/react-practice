a11b877990f76df760422bedf9ad4ad526ed960a
Dima <96godin@gmail.com>
2025-09-13 23:23:32 +0300

    react-edu

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,25 +1,30 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function UserForm() {
-  const [inputName, setInputName] = useState('')
-  const [inputAge , setInputAge] = useState('')
+  const [inputName, setInputName] = useState('');
+  const [inputAge, setInputAge] = useState('');
 
   return (
     <div>
       <input
-       type="text" 
-       value={inputName}
-       onInput={(e) => setInputName(e.target.value)}
-       />
-       <input
-       type="number" 
-       value={inputAge}
-       onChange={(e) => setInputAge(e.target.value)}
-       />
+        placeholder="Name"
+        type="text"
+        value={inputName}
+        onInput={(e) => setInputName(e.target.value)}
+      />
+      <input
+        placeholder="Age"
+        type="number"
+        value={inputAge}
+        onChange={(e) => setInputAge(e.target.value)}
+      />
 
-       <p>Hi, {inputName ? inputName : 'friend!'}! You are {inputAge ? inputAge : 'noAge'} y.o</p>
+      <p>
+        Hi, {inputName ? inputName : 'friend!'}! You are{' '}
+        {inputAge ? inputAge : 'noAge'} y.o
+      </p>
     </div>
-  )
+  );
 }
 
-export default UserForm
+export default UserForm;
